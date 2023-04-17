19776ea60bb9fa257de549fee59f86d66653a53c
thatblueboy <f20212485@goa.bits-pilani.ac.in>
2023-04-17 07:27:58 +0530

    fixed errors

--- reinforce.py
+++ reinforce.py
@@ -7,23 +7,25 @@ import random
 # 2: RIGHT
 # 3: UP
 
-env = gym.make("FrozenLake-v1", desc=["FFFF", "FFFF","FFFF", "SHHG"], render_mode="", is_slippery=False)
 thetas = np.zeros((16, 4), dtype=float)
 pi = np.zeros((16, 4), dtype=float)
+pi[pi == 0] = 0.25
+# print(pi)
 NUMBER_OF_STATES = 16
 NUMBER_OF_ACTIONS = 4
 ALPHA = 0.5
 GAMMA = 0.1
-EPISODES = 1000
+EPISODES = 100
 Returns = []
 
 def getReward(observation):
     if 0 <= observation <= 12:
-        return -1
+        return 1
     if observation == 13 or observation == 14:
-        return -100
+        return 0
     if observation == 15:
-        return 1000
+        return 10
+    
 def findDiscountedReturn(traj, t, T):
     G = 0
     for i in range(t, T):
@@ -47,43 +49,59 @@ def update(traj):
     for t in range(T):
         state, action, reward = traj[t]
         G = findDiscountedReturn(traj, t, T)
-        # print(state)
-        # print(G)
         score = actionToVector(action)-pi[state]
         thetas[state] = thetas[state] + ALPHA*score*G
         print(thetas[state])
         updatePI(state)
 
 def policy(state):
-    return random.choice(random.choices([[0], [1], [2], [3]], weights = list(pi[state]), k = 1000))
+    action = np.random.choice([0, 1, 2, 3], 1, p = pi[state])
+    return int(action)
 
-def fillPI():
-    for i in range(NUMBER_OF_STATES):
-        updatePI(i)
-fillPI()
+def deterministicPolicy(state):
+    prob = max(list(pi[state]))
+    return list(pi[state]).index(prob)
 
+# DISPLAY RANDOM POLICY
+env = gym.make("FrozenLake-v1", desc=["FFFF", "FFFF","FFFF", "SHHG"], render_mode="human", is_slippery=False)
+for episodes in range(5):
+    env.reset()
+    state = 12
+    for timestamp in range(100):
+        action = policy(state)
+        new_state, reward, terminated, truncated, info = env.step(action)
+        env.render()
+        state = new_state
+        if terminated or truncated:
+            break
+
+
+# TRAIN
+env = gym.make("FrozenLake-v1", desc=["FFFF", "FFFF","FFFF", "SHHG"], render_mode="", is_slippery=False)
 for episodes in range(EPISODES):
     env.reset()
     traj = []
-    state = 13
+    state = 12
     print(thetas)
     print(pi)
 
     for timestamp in range(100):
         action = policy(state)
-        # print(action)
-        action = env.action_space.sample()
         new_state, reward, terminated, truncated, info = env.step(action)
         reward = getReward(new_state)
-        # env.render()
         traj.append([state, action, reward])
         state = new_state
-        # print("Reward: {:.2f}".format(reward))
         if terminated or truncated:
             break
     Returns.append(findDiscountedReturn(traj, 0, len(traj)))
     print(traj)
     update(traj)
+
+
+
+
+# DISPLAY RESULT
+
 
 print(thetas)
 print(pi)
@@ -91,9 +109,9 @@ print(pi)
 env = gym.make("FrozenLake-v1", desc=["FFFF", "FFFF","FFFF", "SHHG"], render_mode="human", is_slippery=False)
 for episodes in range(5):
     env.reset()
+    state = 12
     for timestamp in range(100):
-        action = policy(state)
-        action = env.action_space.sample()
+        action = deterministicPolicy(state)
         new_state, reward, terminated, truncated, info = env.step(action)
         env.render()
         state = new_state
